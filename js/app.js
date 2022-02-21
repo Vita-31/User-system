0ab121f3cb08d57a446cd1815fc4cd166ebfa024
Vita Kuznetsova <vita_kuznetsovav@gmail.com>
2022-02-21 19:54:21 +0200

    render users

--- js/app.js
+++ js/app.js
@@ -1,17 +1,44 @@
 const dom = {
-    users: document.getElementById('users'),
+    usersList: document.getElementById('users'),
     create: document.getElementById('create')
+
 }
 
+const users = [
+    {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+          "street": "Kulas Light",
+          "suite": "Apt. 556",
+          "city": "Gwenborough",
+          "zipcode": "92998-3874",
+          "geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496"
+          }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+          "name": "Romaguera-Crona",
+          "catchPhrase": "Multi-layered client-server neural-net",
+          "bs": "harness real-time e-markets"
+        }
+      }
+]
+renderUsers(users, dom.usersList)
+
 //open edit field
-dom.users.addEventListener('click', (e) => {
+dom.usersList.addEventListener('click', (e) => {
     const btnEdit = e.target.closest('.user__btn');
     const userBlock = e.target.closest('.user-data');
     const btnSave = e.target.closest('.btn--save')
     if(btnEdit) {
         userBlock.classList.add('edit-data')
     }
-
     if(btnSave) {
         userBlock.classList.remove('edit-data')
     }
@@ -21,7 +48,7 @@ dom.users.addEventListener('click', (e) => {
 dom.create.addEventListener('click', (e) => {
     const btnCreate = e.target.closest('.btn--create');
     const createBlock = dom.create.querySelector('.create');
-    console.log(createBlock)
+
     if(btnCreate) {
         if(dom.create.classList.contains('open-create')) {
             dom.create.classList.remove('open-create')
@@ -32,3 +59,102 @@ dom.create.addEventListener('click', (e) => {
         }
     }
 })
+
+function createUserHTMl(user) { 
+    return  `
+            <div class="page__user user" id="${user.id}">
+                <div class="user-block">
+                    <div class="user-data">
+                        <p class="user-title">Name:</p>
+                        <div class="user-right">
+                            <p class="user-text">${user.name}</p>
+                            <button class="btn user__btn">
+                                <img src="./img/edit.svg" class="btn__edit" alt="Edit" width="1" height="1" decoding="async">
+                            </button>
+                        </div>
+                        <form class="user__edit form">
+                        <input type="text" placeholder="Enter your name" class="input" required> 
+                        <button class="btn btn--save" type="submit">Save</button>
+                        </form>
+                    </div>
+                    <div class="user-data">
+                        <p class="user-title">Username:</p>
+                        <div class="user-right">
+                            <p class="user-text">${user.username}</p>
+                            <button class="btn user__btn">
+                                <img src="./img/edit.svg" class="btn__edit" alt="Edit" width="1" height="1" decoding="async">
+                            </button>
+                        </div>
+                        <form class="user__edit edit">
+                            <input type="text" placeholder="Enter your name" class="edit__input" required> 
+                            <button class="btn btn--save" type="submit">Save</button>
+                        </form>
+                    </div>
+                    <div class="user-data">
+                        <p class="user-title">Email:</p>
+                        <div class="user-right">
+                            <a href="mailto:Sincere@april.biz" class="user-text">${user.email}</a>
+                            <button class="btn user__btn">
+                                <img src="./img/edit.svg" class="btn__edit" alt="Edit" width="1" height="1" decoding="async">
+                            </button>
+                        </div>
+                        <form class="user__edit edit">
+                            <input type="email" placeholder="Enter your name" class="edit__input" required> 
+                            <button class="btn btn--save" type="submit">Save</button>
+                        </form>
+                    </div>
+                    <div class="user-data">
+                        <p class="user-title">Phone:</p>
+                        <div class="user-right">
+                            <a href="tel:1770736803156442" class="user-text">${user.phone}</a>
+                            <button class="btn user__btn">
+                                <img src="./img/edit.svg" class="btn__edit" alt="Edit" width="1" height="1" decoding="async">
+                            </button>
+                        </div>
+                        <form class="user__edit edit">
+                            <input type="tel" placeholder="Enter your name" class="edit__input" required> 
+                            <button class="btn btn--save" type="submit">Save</button>
+                        </form>
+                    </div>
+                </div>
+                <div class="user-block">
+                    <div class="user-data">
+                        <p class="user-title">Address:</p>
+                        <div class="user-right">
+                            <p class="user-text"> <span>${user.address.street},</span> <span>${user.address.suite},</span> <span>${user.address.city},</span> <span>${user.address.zipcode}</span></p>
+                            <button class="btn user__btn">
+                                <img src="./img/edit.svg" class="btn__edit" alt="Edit" width="1" height="1" decoding="async">
+                            </button>
+                        </div>
+                        <form class="user__edit edit">
+                            <input type="text" placeholder="Enter your name" class="edit__input" required> 
+                            <button class="btn btn--save" type="submit">Save</button>
+                        </form>
+                    </div>
+                    <div class="user-data">
+                        <p class="user-title">Website:</p>
+                        <div class="user-right">
+                            <a href="/hildegard.org" class="user-text">${user.website}</a>
+                            <button class="btn user__btn">
+                                <img src="./img/edit.svg" class="btn__edit" alt="Edit" width="1" height="1" decoding="async">
+                            </button>
+                        </div>
+                        <form class="user__edit edit">
+                            <input type="text" placeholder="Enter your name" class="edit__input" required> 
+                            <button class="btn btn--save" type="submit">Save</button>
+                        </form>
+                    </div>
+                    <button class="btn btn--cancel user__cancel">Delete User</button>
+                </div>
+            </div>
+        `
+}
+
+function createUser(users) {
+    return users.map((user) => createUserHTMl(user)).join('')
+}
+
+function renderUsers(users, usersList) {
+    usersList.innerHTML = createUser(users)
+}
+
